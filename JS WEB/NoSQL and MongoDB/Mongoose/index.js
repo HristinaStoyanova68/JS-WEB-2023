bef096a21fcb1221bef632f60b76d5cc5cb8bc22
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-04 17:58:16 +0300

    creating a new collection

--- JS WEB/NoSQL and MongoDB/Mongoose/index.js
+++ JS WEB/NoSQL and MongoDB/Mongoose/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Cat = require('./models/Cat');
+const Person = require('./models/Person');
 
 const connectionStr = 'mongodb://127.0.0.1:27017';
 
@@ -36,12 +37,39 @@ async function connectDb() {
     // await newCat.save();
 
     //2:
-    const newCat = await Cat.create({
-            name: 'Aya',
-            age: 10,
-            breed: 'Angora'
-        })
-    console.log(newCat);
+    // const newCat = await Cat.create({
+    //         name: 'Aya',
+    //         age: 10,
+    //         breed: 'Angora'
+    //     });
+
+    //update method 1:
+    // const aya = await Cat.findOne({name: 'Aya'});
+    // aya.age = 12;
+    // await aya.save();
+
+    //update method 2: native mongoDb
+    // await Cat.updateOne({name: 'Aya'}, {$set: {age: 13}});
+
+    //update method 3: the mongoose extension
+    // await Cat.findByIdAndUpdate('123456', {$set: {age: 14}});
+
+    //delete method 1:
+    // await Cat.deleteOne({name: 'Aya'});
+
+    //delete method 2:
+    // await Cat.findByIdAndDelete('651d5edffd54507cfb644431');
+
+    //console.log(newCat);
+
+    //creating collection if the first record is non existent collection(creating a new collection)
+    // await Person.create({
+    //     name: 'Pesho',
+    //     age: 25
+    // });
+
+    // await Person.deleteOne({name: 'Pesho'});
+
     
 }
 
