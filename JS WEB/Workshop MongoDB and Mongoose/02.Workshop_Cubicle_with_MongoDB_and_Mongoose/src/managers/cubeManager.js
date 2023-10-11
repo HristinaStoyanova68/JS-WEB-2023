8ea4c8db5496e1a6e5c7f3211a452c15d7fe522b
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-11 22:12:06 +0300

    Get cubes from db

--- JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/managers/cubeManager.js
+++ JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/managers/cubeManager.js
@@ -1,27 +1,9 @@
 const Cube = require('../models/Cube');
 
-const uniqid = require('uniqid');
+exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean();
 
-const cubes = [
-    {
-        id: '1n73sh8holhz66elc',
-        name: 'Mirror Cube',
-        description: 'A cool mirror cube',
-        imageUrl: 'https://logicbg.com/wp-content/uploads/2022/02/rubik-kub-Mirror-Cube-3h3-QiYi-Speed-Cube.jpg.webp',
-        difficultyLevel: 4
-    },
-    {
-        id: '2n73sh8holaz66elc',
-        name: 'Rubik Classic',
-        description: 'Evergreen',
-        imageUrl: 'https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:eco%2Cw_1200/MTk3MDg5MjU5NDA3MDI1MjM1/rubik-cube-algorithms.png',
-        difficultyLevel: 3
-    }
-];
-
-exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
-
+    //TODO: use mongoose to filter in the db
     if (search) {
         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
     }
