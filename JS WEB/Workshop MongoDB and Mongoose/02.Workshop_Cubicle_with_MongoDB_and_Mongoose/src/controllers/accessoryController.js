d5eb26d3594dfdaa5af7bb9c129e1011032a58a2
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-11 22:43:02 +0300

    add post action for create accessory

--- JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/controllers/accessoryController.js
+++ JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/controllers/accessoryController.js
@@ -4,4 +4,11 @@ router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
 
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    //TODO: add accessory data to db
+    res.redirect('/');
+});
+
 module.exports = router;
