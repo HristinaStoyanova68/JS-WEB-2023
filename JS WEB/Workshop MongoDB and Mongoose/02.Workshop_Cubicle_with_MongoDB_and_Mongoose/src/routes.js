93e358d99a4ff7986f9289dfcd08995e3adb03b6
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-11 22:35:13 +0300

    add accessory page

--- JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/routes.js
+++ JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.use(homeController);
 router.use('/cubes', cubeController);
+router.use('/accessories', accessoryController);
 router.get('*', (req,res) => {
     //res.render(404);
     res.redirect('/404');
