d0969409b9ec282bd2f656e391f8b2f314cc7a9b
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-12 10:38:12 +0300

    show remaining accessories only

--- JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/controllers/cubeController.js
+++ JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/controllers/cubeController.js
@@ -38,7 +38,7 @@ router.get('/:cubeId/details', async (req, res) => {
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-    const accessories = await accessoryManager.getAll().lean();
+    const accessories = await accessoryManager.getRest(cube.accessories).lean();
 
     const hasAccessories = accessories.length > 0;
 
--- JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/managers/accessoryManager.js
+++ JS WEB/Workshop MongoDB and Mongoose/02.Workshop_Cubicle_with_MongoDB_and_Mongoose/src/managers/accessoryManager.js
@@ -3,3 +3,5 @@ const Accessory = require('../models/Accessory');
 exports.getAll = () => Accessory.find();
 
 exports.create = (accessorydata) => Accessory.create(accessorydata);
+
+exports.getRest = (accessoryIds) => Accessory.find({_id: { $nin: accessoryIds }});
