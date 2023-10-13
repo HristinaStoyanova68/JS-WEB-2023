8aad649a609e0e05afea1721e4402e262ad87fb1
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-13 16:28:53 +0300

    save user with validated password

---
+++ JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: {
+        type: String,
+        // validate: {
+        //     validator: function(value) {
+        //         return this.repeatPassword === value;
+        //     },
+        //     message: 'Password missmatch!',
+        // }
+    },
+});
+
+userSchema.virtual('repeatPassword').set(function(value) {
+    if (value !== this.password) {
+        throw new mongoose.MongooseError('Password missmatch!');
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
