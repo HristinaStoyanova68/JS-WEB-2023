12a74869b0d7ffad420f9ca99290dae4a607a39c
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-13 18:14:49 +0300

    user login

--- JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/managers/userManager.js
+++ JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/managers/userManager.js
@@ -1,3 +1,25 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    //TODO find user
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Cannot find username or password!');
+    }
+
+    //TODO validate password
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Cannot find username or password!');
+    }
+
+    //TODO return user
+    return user;
+};
+
--- JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/models/User.js
+++ JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/models/User.js
@@ -14,6 +14,8 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+//TODO validate if user exist
+
 userSchema.virtual('repeatPassword').set(function(value) {
     if (value !== this.password) {
         throw new mongoose.MongooseError('Password missmatch!');
