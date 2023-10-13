cc26db721f33307ff9ee0b2f10f88d176a68b323
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-13 16:53:12 +0300

    hash password before save

--- JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/models/User.js
+++ JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -17,7 +18,13 @@ userSchema.virtual('repeatPassword').set(function(value) {
     if (value !== this.password) {
         throw new mongoose.MongooseError('Password missmatch!');
     }
-})
+});
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+});
 
 const User = mongoose.model('User', userSchema);
 
