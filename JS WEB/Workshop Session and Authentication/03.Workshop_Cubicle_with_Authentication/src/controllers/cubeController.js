51eaff691e94a90ee2173953b6b4d315f23d2870
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-15 00:21:31 +0300

    add delete cube page

--- JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/controllers/cubeController.js
+++ JS WEB/Workshop Session and Authentication/03.Workshop_Cubicle_with_Authentication/src/controllers/cubeController.js
@@ -7,7 +7,7 @@ const accessoryManager = require('../managers/accessoryManager');
 router.get('/create', (req, res) => {
     // console.log(cubeManager.getAll());
     // console.log(req.user);
-    res.render('create');
+    res.render('/cube/create');
 });
 
 router.post('/create', async (req, res) => {
@@ -35,7 +35,7 @@ router.get('/:cubeId/details', async (req, res) => {
     if (!cube) {
         return res.redirect('/404');
     }
-    res.render('details', { cube });
+    res.render('cube/details', { cube });
 });
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
@@ -55,6 +55,13 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
     await cubeManager.attachAccessory(cubeId, accessoryId);
 
     res.redirect(`/cubes/${cubeId}/details`);
-})
+});
+
+router.get('/:cubeId/delete', async (req, res) => {
+
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    
+    res.render('cube/delete', { cube });
+});
 
 module.exports = router;
