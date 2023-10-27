b8b7d0fa8fc97efb2e37970d5bf5f90a6cfe4ee8
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-27 15:46:07 +0300

    state demo

--- ReactJS/Component_Basic/components_basic/src/App.jsx
+++ ReactJS/Component_Basic/components_basic/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import MovieList from './components/MovieList';
 import movies from './assets/movies';
+import Timer from './components/Timer';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -9,6 +10,11 @@ function App() {
    return (
     <div>
       <h1>My First Dynamic React Application</h1>
+
+      {/* <Timer startTime={5}/>
+      <Timer startTime={1}/>
+      <Timer startTime={2}/>
+      <Timer startTime={3}/> */}
 
       <MovieList movies={movies} headingText="Movie List" />
     </div>
