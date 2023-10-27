4f1c1a33c763223f7b0ddde7850e72e0db8b2fd6
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-10-27 17:17:48 +0300

    using switch operator

--- ReactJS/Component_Basic/components_basic/src/components/Counter.jsx
+++ ReactJS/Component_Basic/components_basic/src/components/Counter.jsx
@@ -25,6 +25,28 @@ export default function Counter(props) {
     // if (count < 0) {
     //     warning = <p>Invalid Counter</p>
     // }
+    let message = null;
+
+    switch (count) {
+        case 1:
+            message = 'One'
+            break;
+        case 2:
+            message = 'Two'
+            break;
+        case 3:
+            message = 'Three'
+            break;
+        case 4:
+            message = 'Four'
+            break;
+        case 5:
+            message = 'Five'
+            break;
+        default:
+            message = 'Keep Going'
+            break;
+    }
 
     return (
         <div>
@@ -32,17 +54,18 @@ export default function Counter(props) {
 
             {/* {warning} */}
 
-        {/* use ternary operator: */}
+            {/* use ternary operator: */}
             {count < 0
-            ? <p>Invalid Counter</p>
-            : <p>Valid counter</p>
+                ? <p>Invalid Counter</p>
+                : <p>Valid counter</p>
             }
 
             {/* use boolean operators */}
             {count == 0 && <p>Please start Incrementing</p>}
 
+            <h4>{message}</h4>
 
-            <p>Count: { count }</p>
+            <p>Count: {count}</p>
 
             <button onClick={() => setCount(count - 1)}>-</button>
             <button onClick={clearCounterHandler}>clear</button>
