54a4fcb550c803d6f3cc469c0e6dac8ebdbb37af
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-08 17:33:56 +0200

    update base components in ControlledForm.jsx

--- ReactJS/React_Forms/src/components/ControlledForm.jsx
+++ ReactJS/React_Forms/src/components/ControlledForm.jsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+
 export default function ControlledForm () {
+const [usernameValue, setUsernameValue] = useState('');
+const [passwordValue, setPasswordValue] = useState('');
+const [ageValue, setAgeValue] = useState('');
+
+const usernameChangeHandler = (e) => {
+    setUsernameValue(e.target.value);
+}
+
+const resetFormHandler = () => {
+    setUsernameValue('');
+}
+
+const usernameBlurHandler = () => {
+    console.log('on blur');
+}
+
+
+const passwordChangeHandler = (e) => {
+    setPasswordValue(e.target.value);
+}
+
+const passwordBlurHandler = () => {
+    console.log('ON BLUR');
+}
+
+const ageChangeHandler = (e) => {
+    setAgeValue(Number(e.target.value));
+}
+
+const ageBlurHandler = () => {
+    console.log('On Blur');
+}
+
+console.log(usernameValue);
+console.log(passwordValue);
+console.log(ageValue);
+
     return (
         <>
             <h1>Controlled Form</h1>
@@ -6,19 +45,40 @@ export default function ControlledForm () {
             <form>
                 <div>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" />
+                    <input 
+                        type="text" 
+                        name="username" 
+                        id="username" 
+                        value={usernameValue}
+                        onChange={usernameChangeHandler}
+                        onBlur={usernameBlurHandler}
+                    />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        value={passwordValue}
+                        onChange={passwordChangeHandler}
+                        onBlur={passwordBlurHandler}
+                    />
                 </div>
                 <div>
                     <label htmlFor="age">Age</label>
-                    <input type="number" name="age" id="age" />
+                    <input 
+                        type="number" 
+                        name="age" 
+                        id="age"
+                        value={ageValue} 
+                        onChange={ageChangeHandler}
+                        onBlur={ageBlurHandler}
+                    />
                 </div>
                 <div>
                     <button>Register</button>
-                    <button type="button">Reset</button>
+                    <button type="button" onClick={resetFormHandler}>Reset</button>
                 </div>
 
             </form>
