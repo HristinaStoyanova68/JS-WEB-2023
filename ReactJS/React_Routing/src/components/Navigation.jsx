e33770d5b9ca109c2e5a17b49676bfc7d1c370e6
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-09 19:41:25 +0200

    add Link component instead a tag

--- ReactJS/React_Routing/src/components/Navigation.jsx
+++ ReactJS/React_Routing/src/components/Navigation.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Navigation = () => {
     return (
         <nav>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contacts">Contacts</Link>
+            {/* <a href="/">Home</a>
             <a href="/about">About</a>
-            <a href="/contacts">Contacts</a>
+            <a href="/contacts">Contacts</a> */}
         </nav>
     );
 }
