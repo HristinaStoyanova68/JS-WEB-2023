826f0e56cadf3850bd90d243d38aaa64480ca719
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-17 14:36:02 +0200

    create header component

--- ReactJS/React_Routing_Workshop/client/src/App.jsx
+++ ReactJS/React_Routing_Workshop/client/src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/header/Header";
 
 function App() {
 
   return (
-    <>
-      <h1>Hello Vite</h1>
-    </>
+    <div id="box">
+      <Header />
+    </div>
   )
 }
 
