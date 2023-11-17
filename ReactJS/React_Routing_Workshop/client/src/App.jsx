1a54783ef2565592e61a03c630320aa40412eacf
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-17 15:43:39 +0200

    add GameCreate component

--- ReactJS/React_Routing_Workshop/client/src/App.jsx
+++ ReactJS/React_Routing_Workshop/client/src/App.jsx
@@ -3,19 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GameList from './components/game-list/GameList';
+import GameCreate from './components/game-create/GameCreate';
 
 function App() {
 
-  return (
-    <div id="box">
-      <Header />
+    return (
+        <div id="box">
+            <Header />
             <Routes>
-                <Route path='/' element={ <Home /> } />
-                <Route path='/games' element={ <GameList/>} />
+                <Route path='/' element={<Home />} />
+                <Route path='/games' element={<GameList />} />
+                <Route path='/games/create' element={<GameCreate />} />
             </Routes>
-      
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default App
