1834f563ea8f91278b7923ef90f777ff7b6261df
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-18 09:59:23 +0200

    show games dinamic in gameList

---
+++ ReactJS/React_Routing_Workshop/client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+export default function GameListItem({
+    title,
+    category,
+    imageUrl
+}) {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={imageUrl} />
+                <h6>{category}</h6>
+                <h2>{title}</h2>
+                <Link to="#" className="details-button">Details</Link>
+            </div>
+        </div>
+    );
+}
