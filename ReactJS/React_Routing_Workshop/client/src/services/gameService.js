b69d4ff90d1c5966c2bd16e928189ff96f1e5af0
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-18 13:43:47 +0200

    create abstract requester

--- ReactJS/React_Routing_Workshop/client/src/services/gameService.js
+++ ReactJS/React_Routing_Workshop/client/src/services/gameService.js
@@ -1,9 +1,9 @@
-import { request } from "../components/lib/request";
+import * as request from "../components/lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
 
     // console.log(result);
 
@@ -11,15 +11,18 @@ export const getAll = async () => {
 }
 
 export const create = async (gameData) => {
-    const response = await fetch (baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(gameData),
-    });
+    const result = await request.post(baseUrl, gameData);
 
-    const result = await response.json();
+    //without using request function
+    // const response = await fetch (baseUrl, {
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify(gameData),
+    // });
+
+    // const result = await response.json();
 
     return result;
 }
