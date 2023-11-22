caccbd7a69c1952378e4c71fc1463c08f18ea5a3
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-23 00:51:06 +0200

    add register function to authService

--- ReactJS/React_Routing_Workshop/client/src/services/authService.js
+++ ReactJS/React_Routing_Workshop/client/src/services/authService.js
@@ -3,11 +3,20 @@ import * as request from '../lib/request';
 const baseUrl = 'http://localhost:3030/users';
 
 export const login = async (email, password) => {
-    console.log(baseUrl);
+    // console.log(baseUrl);
     const result = await request.post(`${baseUrl}/login`, {
         email,
         password,
     });
 
     return result;    
+};
+
+export const register = (email, password) => {
+    const result = request.post(`${baseUrl}/register`, {
+        email,
+        password,
+    });
+
+    return result;
 }
